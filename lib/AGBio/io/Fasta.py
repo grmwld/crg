b2653facdb3300c697412fc171cb13e500915e4e
Alexis Grimaldi <sopo.dev@gmail.com>
2010-01-11 18:54:56 +0100

    New class Alignment to handle alignments in fasta format.
    The sequence attribute of a Sequence object is now unsplit.
    Sequence object can be printed using prints()
    Rewrote the save() method of SequenceList to prints(), using the prints()
    method of each Sequence object in the SequenceList.

--- lib/AGBio/io/Fasta.py
+++ lib/AGBio/io/Fasta.py
@@ -10,6 +10,8 @@ class SequenceList(list):
     '''
     def __init__(self, sequences=''):
         self.extend(sequences)
+        ## for backward compatibility:
+        self.save = self.prints
 
     def findPattern( sequences, pattern, mode='count' ):
         '''Finds all sequences in the list with sequence matching a pattern.
@@ -50,36 +52,6 @@ class SequenceList(list):
                     usequences.append(seq)
         return usequences
 
-    def findPositions(self, aaa=None, strict=False):
-        '''Finds all positions of U in the alignment.
-        '''
-        aa = list(aaa)
-#        aa.append('!')
-        print aa
-        output = {}
-        tmppos = {}
-        for i in aa:
-            output[i] = {}
-            tmppos[i] = []
-        for seq in self:
-            for i in aa:
-                tmppos[i] = []
-            for i, pos in enumerate(seq.sequence):
-                if pos in aa:
-                    tmppos[pos].append(i)
-            for t in tmppos:
-                if tuple(tmppos[t]) in output[t]:
-#                    if tmppos[t]:
-                        output[t][tuple(tmppos[t])].append(seq)
-#                    else:
-#                        output['!'][tuple(tmppos[t])].append(seq)
-                else:
-#                    if tmppos[t]:
-                        output[t][tuple(tmppos[t])] = [seq]
-#                    else:
-#                        output['!'][tuple(tmppos[t])] = [seq]
-        return output
-
     def symetric_difference(self, other, method='formated'):
         '''Find elements in either the list or other but not both.
         '''
@@ -98,14 +70,81 @@ class SequenceList(list):
             sys.exit("Comparision method sould be one of ('formated', 'raw')")
         return seqlist
 
-    def save(self, outfile):
-        '''Writes the sequences in fasta format in the specified output file.
+##     def save(self, outfile):
+##         '''Writes the sequences in fasta format in the specified output file.
+##         '''
+##         for sequence in self:
+##             outfile.write( sequence.header )
+##             outfile.write('\n')
+##             outfile.write( sequence.sequence )
+##             outfile.write('\n')
+
+    def prints(self, outfile=sys.stdout, length=80):
+        for seq in self:
+            seq.prints(outfile, length)
+
+
+class Alignment(SequenceList):
+    '''Alignment class.
+    '''
+    def __init__(self, sequences=''):
         '''
-        for sequence in self:
-            outfile.write( sequence.header )
-            outfile.write('\n')
-            outfile.write( sequence.sequence )
-            outfile.write('\n')
+        '''
+        SequenceList.__init__(self, sequences)
+        for i, seq in enumerate(self):
+            for sseq in self[i:]:
+                if len(seq) != len(sseq):
+                    raise Exception()
+        self.length = len(self[0].sequence)
+        self.profiles=[]
+
+    def getProfiles(self):
+        for pos in range():
+            pass
+
+    def findPositions(self, aaa=None, redundant=True):
+        '''Finds all positions of U in the alignment.
+        '''
+        aa = list(aaa)
+        output = {}
+        if not redundant:
+            tmppos = {}
+            for i in aa:
+                output[i] = {}
+                tmppos[i] = []
+            for seq in self:
+                for i in aa:
+                    tmppos[i] = []
+                for i, pos in enumerate(seq.sequence):
+                    if pos in aa:
+                        tmppos[pos].append(i)
+                for t in tmppos:
+                    if tuple(tmppos[t]) in output[t]:
+                            output[t][tuple(tmppos[t])].append(seq)
+                    else:
+                            output[t][tuple(tmppos[t])] = [seq]
+        else:
+            for i in aa:
+                output[i] = {}
+            for seq in self:
+                for p in aa:
+                    foundany = False
+                    for i, pos in enumerate(seq.sequence):
+                        if pos == p:
+                            foundany = True
+                            try:
+                                if tuple([i]) in output[p]:
+                                    output[p][tuple([i])].append(seq)
+                                else:
+                                    output[p][tuple([i])] = [seq]
+                            except TypeError:
+                                print pos, p, i, output[p]
+                    if not foundany:
+                        if () in output[p].keys():
+                            output[p][()].append(seq)
+                        else:
+                            output[p][()] = [seq]
+        return output
 
 
 class Sequence(object):
@@ -113,10 +152,13 @@ class Sequence(object):
     '''
     def __init__(self, header, sequence):
         self.__header = header
-        self.__sequence = sequence
+        self.__sequence = ''.join(sequence.split())
 
     def __str__(self):
         return str([self.header, self.sequence])
+
+    def __len__(self):
+        return len(self.sequence)
 
     @property
     def header(self):
@@ -148,6 +190,16 @@ class Sequence(object):
                 tmp += c
         return Sequence(self.header, tmp)
 
+    def prints(self, outfile=sys.stdout, length=80):
+        if length > 0:
+            llength = length
+        else:
+            llength = len(self)
+        outfile.write(self.header)
+        outfile.write('\n')
+        for buf in range(0, len(self), llength):
+            outfile.write(self.sequence[buf:buf+llength])
+            outfile.write('\n')
 
 def loadSequences( infile ):
     '''
@@ -163,7 +215,7 @@ def loadSequences( infile ):
                 inseq = False
             inseq = True
         if inseq:
-            tmp.append( line )
+            tmp.append( line.strip() )
     if tmp:
         sequences.append( Sequence( tmp[0].strip(), ''.join( tmp[1:] ).strip() ) )
     return sequences
@@ -208,20 +260,19 @@ def replacePattern( sequence, pattern, replacement ):
 
 if __name__ == '__main__':
 
-    with open('/users/rg/agrimaldi/Data/gos/selenoprofiles_profiles/dio_like.det.fasta', 'r') as ff:
-        ss = loadSequences(ff)
-    uu = ss.findPositions(('U'))
 
-    #print uu
+    with open('/users/rg/agrimaldi/Data/gos/selenoprofiles_profiles/dio_like.det.fasta', 'r') as ff:
+        aa = loadSequences(ff)
+    ss = Alignment(aa)
+    uu = ss.findPositions(('U','C','-'), True)
+
     count = 0
     for i in uu:
         for j in uu[i]:
-            if len(uu[i][j]) > 20:
+            if len(uu[i][j]) > 200:
                 print i, j, len(uu[i][j])
             count += len(uu[i][j])
-
+    
     print count, len(ss)
-    for i in uu['U'][(1687, 4679)]:
-        print i.header
-    for i in uu['U'][(1687, 4926)]:
-        print i.header
+
+    print len(uu['U'][(1687,)]), len(uu['C'][(1687,)]), len(uu['-'][(1687,)])
