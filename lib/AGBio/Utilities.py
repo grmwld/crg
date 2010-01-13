b5e20eb3075c59e28a3e80fc0611f4848e6671f1
Alexis Grimaldi <sopo.dev@gmail.com>
2010-01-13 18:56:20 +0100

    Added a custom getch() function.

--- lib/AGBio/Utilities.py
+++ lib/AGBio/Utilities.py
@@ -2,9 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 def getBaseName(filename, numextension=1):
     return '.'.join(filename.split('.')[:-numextension])
 
 def changeFileExtension(filename, newextension, numextension=1):
     return getBaseName(filename, numextension) + '.' + newextension
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self, msg=None):
+        import sys, tty, termios
+        sys.stdout.write(msg+'\n')
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+getch = _GetchUnix()
