169b1732fffbfa845dc6295276a5faf7be105d22
Alexis Grimaldi <sopo.dev@gmail.com>
2010-02-02 19:15:31 +0100

    Added a layout function to customize the tree.
    - biggers balls if leaves.
    - play around with long/short/short names.

--- scripts/gen_tree.py
+++ scripts/gen_tree.py
@@ -4,7 +4,25 @@
 import sys
 import os
 import optparse
-from ete2 import Tree
+from ete2 import Tree, faces
+
+def long2short(longname):
+    ln = longname.split()
+    return ln[0][0] + ln[1][:3]
+
+def layout(node):
+    if node.is_leaf():
+        node.img_style['size'] = 10
+        shortNameFace = faces.TextFace(long2short(node.name))
+        longNameFace = faces.TextFace(node.name)
+#        faces.add_face_to_node(longNameFace, node, column=0)
+        faces.add_face_to_node(shortNameFace, node, column=0)
+        
+        if node.name.split()[1].startswith('m'):
+            node.img_style["fgcolor"] = "#FFFFFF"
+    else:
+        node.img_style['size'] = 0
+        
 
 def main():
 
@@ -27,7 +45,7 @@ def main():
     t = Tree(options.infilename)
 
     if options.gui:
-        t.show()
+        t.show(layout)
     else:
         print t
 
