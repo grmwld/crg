1016b103a3fb1bca8d6cfabf42cb8140318e9e14
Alexis Grimaldi <sopo.dev@gmail.com>
2010-02-03 17:52:07 +0100

    This module provides some interfaces to analyse data spitted by selenoprofiles.
    For now, it just looks at one directory and gather relevant files together in different dictionaries.
    This allows for intersection of information, facilitating later searches and comparisions.

---
+++ lib/AGBio/selenoprofiles_tools/results_analyser.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python2.6
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+
+class GenomeFolderParser(object):
+
+    def __init__(self, path, dirs=['output'], everywhere=False):
+        self.rootdir = os.path.abspath(path)
+        if everywhere:
+            self.dirs = [os.path.join(self.rootdir, 'output'),
+                         os.path.join(self.rootdir, 'discarded')]
+        else:
+            self.dirs = [os.path.join(self.rootdir, d) for d in dirs]
+        self.sec = {}
+        self.cys = {}
+        self.thr = {}
+        self.arg = {}
+        self.stdsecis = {}
+        self.nonstdsecis = {}
+        self.kw2dict = {'cysteine' : self.cys,
+                        'selenocysteine' : self.sec,
+                        'threonine' : self.thr,
+                        'arginine' : self.arg}
+
+    def parse(self, sec=True, cys=True, thr=True, arg=True, stdsecis=False, nonstdsecis=False):
+        lukw = []
+        if sec:
+            lukw.append('selenocysteine')
+        if cys:
+            lukw.append('cysteine')
+        if thr:
+            lukw.append('threonine')
+        if arg:
+            lukw.append('arginine')
+
+        for d in self.dirs:
+            resfiles = [f for f in os.listdir(d) if self._keep(f)]
+            for ff in resfiles:
+                for kw in lukw:
+                    if kw in ff.split('.'):
+                        self._update_dict(kw, ff)
+
+    def _keep(self, filename):
+        trash = ['.ali']
+        for i in trash:
+            if filename.endswith(i):
+                return False
+        return True
+
+    def _get_hit_num(self, filename, case):
+        if case not in ['cysteine', 'threonine',
+                        'uga_containing', 'selenocysteine',
+                        'unaligned']:
+            raise 'Unknown case : ' + case
+        ff = filename.split('.')
+        index = ff.index(case) - 1
+        return int(ff[index])
+
+    def _update_dict(self, keyword, filename):
+        t_dict = {}
+        t_dict.update(self.kw2dict[keyword])
+
+        protname = filename.split('.')[0]
+        hit_num = self._get_hit_num(filename, keyword)
+        if protname in t_dict:
+            if hit_num in t_dict[protname]:
+                t_dict[protname][hit_num].append(filename)
+            else:
+                t_dict[protname][hit_num] = []
+        else:
+            t_dict[protname] = {}
+
+        self.kw2dict[keyword].update(t_dict)
+
+class ResultFolderParser(object):
+    pass
+
+
+def main():
+
+    testpath = '/users/rg/agrimaldi/Data/gos/selenoprofiles_U_runs/2010_01_28_bact_U_failed/run/Zymomonas_mobilis_subsp._mobilis_ZM4_'
+
+    parser = GenomeFolderParser(testpath)
+
+    parser.parse()
+
+    for i, j in parser.sec.items():
+        print i, j
+    for i, j in parser.cys.items():
+        print i, j
+    for i, j in parser.arg.items():
+        print i, j
+    for i, j in parser.thr.items():
+        print i, j
+
+
+if __name__ == '__main__':
+    main()
