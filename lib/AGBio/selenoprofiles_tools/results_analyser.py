70bcd3cc77d9c826ced204d4aa0206230264689f
Alexis Grimaldi <sopo.dev@gmail.com>
2010-02-04 19:19:00 +0100

    Added a parser for p2g files.

--- lib/AGBio/selenoprofiles_tools/results_analyser.py
+++ lib/AGBio/selenoprofiles_tools/results_analyser.py
@@ -3,10 +3,97 @@
 
 import os
 import sys
+import subprocess
+sys.path.append('/users/rg/mmariotti/libraries')
+from MMlib import bbash
+from AGBio.UtilityWrappers import *
+from AGBio.io.common import *
 
+
+class P2G_Parser(object):
+    '''Parse a p2g output file, using appropriate parsing method
+    according to where it comes from (genewise or exonerate).
+    '''
+    def __init__(self, filename):
+        self._filename = filename
+        self.parse = self._source_prog()
+        self.result = ParserResult()
+
+    def _parse_genewise(self):
+        print self._filename
+        #proc = subprocess.Popen(['parse_genewise.py', '-i', self._filename],
+        #                        stdout=subprocess.PIPE,
+        #                        shell=True)
+        op = bbash(' '.join(['parse_genewise.py', '-i', self._filename]), True)
+        #raw_input()
+        #os.system(' '.join(['parse_genewise.py', '-i', self._filename]))
+        output = [c.strip().split() for c in op.split(';')]
+        self.result.query.parse(output[0], output[5][1])
+        self.result.target.parse(output[1], output[5][2])
+        self.result.frameshifts = output[2][1]
+        self.result.orig_pos = output[3]
+        self.result.score = output[4][1]
+        if len(output[6]) == 1:
+            self.result.stop_codons = 0
+        else: self.result.stop_codons = output[6][1]
+
+    def _parse_exonarate(self):
+        pass
+
+    def _source_prog(self):
+        with open(self._filename, 'r') as iff:
+            for line in iff:
+                if line.strip().startswith('genewise output'):
+                     return self._parse_genewise
+        return self._parse_exonarate
+
+
+class ParserResult(object):
+    '''Class to hold results from parsing.
+    '''
+    def __init__(self):
+        self.score = 0
+        self.introns = 0
+        self.frameshifts = 0
+        self.query = self._result_seq()
+        self.target = self._result_seq()
+        self.orig_pos = []
+        self.consensus = ''
+
+    def __str__(self):
+        return '\n'.join((str(self.query), str(self.target), self.score,
+                          self.frameshifts, str(self.orig_pos)))
+
+    class _result_seq(object):
+        def __init__(self):
+            self.name = ''
+            self.start = 0
+            self.end = 0
+            self.sequence = ''
+        def __str__(self):
+            return '\n\t'.join((self.name, self.start, self.end, self.sequence))
+        def parse(self, info, seq):
+            self.name = info[0]
+            self.start = info[1]
+            self.end = info[2]
+            self.sequence = seq
+            
 
 class GenomeFolderParser(object):
+    '''Parser of a genome folder (from selenoprofiles)
 
+    Parses a set of folder inside the organisme folder.
+    Typically, the folder to investigate is `output`,
+    but could be `discarded`.
+
+    Usage:
+
+    parser = GenomeFolderParser('/path/to/folder')
+    parser.parse(doall=True)
+    print parser.cys
+    print parser.sec
+    ...
+    '''
     def __init__(self, path, dirs=['output'], everywhere=False):
         self.rootdir = os.path.abspath(path)
         if everywhere:
@@ -37,6 +124,24 @@ class GenomeFolderParser(object):
               sec=False, cys=False, thr=False, arg=False,
               uga=False, ual=False,
               stdsecis=False, nonstdsecis=False, twilsecis='False'):
+        '''Method to parse the folder.
+
+        The wanted informations are given as arguments. If `doall` is set to
+        True, everything is parsed.
+
+        Arguments:
+        - `self`:
+        - `doall`:
+        - `sec`:
+        - `cys`:
+        - `thr`:
+        - `arg`:
+        - `uga`:
+        - `ual`:
+        - `stdsecis`:
+        - `nonstdsecis`:
+        - `twilsecis`:
+        '''
         lukw = []
         if doall:
             lukw.append('selenocysteine')
@@ -97,6 +202,7 @@ class GenomeFolderParser(object):
             t_dict[protname] = {hit_num : [filename]}
         self.kw2dict[keyword].update(t_dict)
 
+
 class ResultFolderParser(object):
     pass
 
@@ -108,7 +214,13 @@ def main():
     parser = GenomeFolderParser(testpath)
 
     parser.parse(doall=True)
-    print parser.secis_std
+    print parser.cys
+    pp = os.path.join(testpath, 'output', 'seld.1.unaligned.p2g')
+    print pp
+    pparser = P2G_Parser(pp)
+    pparser.parse()
+    print pparser.result
+    
 
 
 if __name__ == '__main__':
