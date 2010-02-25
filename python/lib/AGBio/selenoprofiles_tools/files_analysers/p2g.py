08d04cd17f67f881a153efce39b276e4b93bc7c4
Alexis Grimaldi <sopo.dev@gmail.com>
2010-02-25 13:43:33 +0100

    Computes coverage of the query for a hit.

--- python/lib/AGBio/selenoprofiles_tools/files_analysers/p2g.py
+++ python/lib/AGBio/selenoprofiles_tools/files_analysers/p2g.py
@@ -5,10 +5,12 @@ from __future__ import with_statement
 import os
 import sys
 import subprocess
+import cStringIO
 sys.path.append('/users/rg/mmariotti/libraries')
 from MMlib import bbash
 from AGBio.io.common import *
-
+from AGBio.ncbi.BlastWrappers import *
+from AGBio.io.Fasta import *
 
 class P2G_Parser(object):
     '''Parse a p2g output file, using appropriate parsing method
@@ -25,10 +27,9 @@ class P2G_Parser(object):
         #                        shell=True)
         op = bbash(' '.join(['parse_genewise.py', '-i', self._filename]), True)
         #raw_input()
-        #os.system(' '.join(['parse_genewise.py', '-i', self._filename]))
-        output = [c.strip().split() for c in op.split(';')]
-        self.result.query.parse(output[0], output[5][1])
-        self.result.target.parse(output[1], output[5][2])
+        output = [c.split() for c in op.split(';')]
+        self.result.query.parse(output[0], output[5][1].strip(','), coverage=True)
+        self.result.target.parse(output[1], output[5][2].strip(','))
         self.result.frameshifts = output[2][1]
         self.result.orig_pos = output[3]
         self.result.score = output[4][1]
@@ -58,7 +59,6 @@ class P2G_ParserResult(object):
         self.target = self._result_seq()
         self.orig_pos = []
         self.consensus = ''
-        self.coverage = 0
 
     def __str__(self):
         return '\n'.join((str(self.query), str(self.target), self.score,
@@ -69,14 +69,36 @@ class P2G_ParserResult(object):
             self.name = ''
             self.start = 0
             self.end = 0
+            self.coverage = ''
             self.sequence = ''
         def __str__(self):
             return '\n'.join((self.name,
-                              self.start + ' --- ' + self.end,
-                              formatText(self.sequence, 50, 4)))
-        def parse(self, info, seq):
+                              self.start + ' - ' + self.end + \
+                              (' | ' + str(round(self.coverage, 3)) \
+                               if self.coverage else ''),
+                              formatText(self.sequence, 60, 4)))
+        def _get_coverage(self, header):
+            local_length = int(self.end) - int(self.start)
+            tmp = header.split('/')
+            if len(tmp) > 1:
+                start_end = [int(n) for n in tmp[1].split('-')]
+                ori_length = float(start_end[1] - start_end[0])
+            else:
+                gi = header.split('|')[1]
+                oo = os.path.join('/home/agrimaldi/temp/', gi)
+                fetcher = FastaCmdWrapper([gi],
+                                          db='/seq/databases/nr_uncompressed/nr',
+                                          outfile=oo)
+                fetcher.run()
+                with open(oo) as iff:
+                    tmp_seq = loadSequences(iff)
+                os.remove(oo)
+                ori_length = float(len(tmp_seq[0].sequence))
+            return local_length / ori_length
+        def parse(self, info, seq, coverage=False):
             self.name = info[0]
             self.start = info[1]
             self.end = info[2]
+            if coverage: self.coverage = self._get_coverage(info[0])
             self.sequence = seq
             
