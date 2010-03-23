5b4c6d16e017f354a17167de9e2e0ced19816672
Alexis Grimaldi <sopo.dev@gmail.com>
2010-03-23 14:45:57 +0100

    Various changes.

--- python/scripts/biochunks/bc_filter_X_fastq.py
+++ python/scripts/biochunks/bc_filter_X_fastq.py
@@ -4,22 +4,21 @@ import os
 import sys
 import optparse
 
-def filter_X(chunks, char='N', max_N=0):
+def filter_X(lines, char='N', max_N=0):
     '''Filters chunks based on the composition of their sequences
-    TODO : Make it more modular so that only the result of a function
-    would be required to filter out the chunks
     '''
-    for chunk in chunks:
-        if chunk[1].count(char) <= max_N and chunk[5].count(char) <= max_N:
-            yield chunk
-
-def chop_reads(lines):
     for line in lines:
-        if line.startswith('@'):
+        if line[0] == '@':
             chunk = [line]
-            for i in xrange(7):
-                chunk.append(lines.next())
-            yield chunk
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            chunk.append(lines.next())        
+            if chunk[1].count(char) <= max_N and chunk[5].count(char) <= max_N:
+                yield chunk
 
 def main():
 
@@ -38,13 +37,12 @@ def main():
 
     (opt, args) = parser.parse_args()
 
-    chunks = filter_X(chop_reads(sys.stdin.xreadlines()),
+    chunks = filter_X(sys.stdin.xreadlines(),
                       opt.forbiden_char, opt.max_occurence)
 
     for chunk in chunks:
-        for line in chunk:
-            sys.stdout.write(line)
-
+        map(sys.stdout.write, chunk)
+ 
 
 if __name__ == '__main__':
     main()
